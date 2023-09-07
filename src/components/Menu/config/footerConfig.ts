82e62a52d5947dcfed72fe400d741fc3043ab715
ODiSWAP <110659576+LandChronicle@users.noreply.github.com>
2023-09-07 17:20:50 +0900

    Update footerConfig.ts

--- src/components/Menu/config/footerConfig.ts
+++ src/components/Menu/config/footerConfig.ts
@@ -10,14 +10,6 @@ export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
          href: 'https://chronicle-land.gitbook.io/kr-land-chronicle/odiswap/contact',
          isHighlighted: true,
        },
-       //{
-       //  label: t('Brand'),
-        // href: 'https://chronicle-land.gitbook.io/kr-land-chronicle/odiswap/undefined',
-       //},
-       //{
-        // label: t('Blog'),
-         //href: 'https://medium.com/pancakeswap',
-       //},
        {
          label: t('DISCORD'),
          href: 'https://discord.gg/landchronicle',
@@ -29,52 +21,13 @@ export const footerLinks: (t: ContextApi['t']) => FooterLinkType[] = (t) => [
        {
          label: '—',
        },
-      // {
-        // label: t('Online Store'),
-        // href: 'https://pancakeswap.creator-spring.com/',
-       //},
     ],
-  },
-  //{
-    //label: t('Help'),
-    //items: [
-     //  {
-      //   label: t('Customer Support'),
-      //   href: 'https://docs.pancakeswap.finance/contact-us/customer-support',
-      // },
-      // {
-      //   label: t('Troubleshooting'),
-      //   href: 'https://docs.pancakeswap.finance/help/troubleshooting',
-      // },
-      // {
-      //   label: t('Guides'),
-      //   href: 'https://docs.pancakeswap.finance/get-started',
-      // },
-   // ],
-  //},
-  {
     label: t('Developers'),
     items: [
-       //{
-        // label: 'Github',
-         //href: 'https://github.com/pancakeswap',
-      // },
        {
          label: t('Documentation'),
          href: 'https://chronicle-land.gitbook.io/kr-land-chronicle/odiswap/undefined',
        },
-       //{
-        // label: t('Bug Bounty'),
-        // href: 'https://docs.pancakeswap.finance/code/bug-bounty',
-       //},
-      // {
-       //  label: t('Audits'),
-        // href: 'https://docs.pancakeswap.finance/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited',
-       //},
-      // {
-       //  label: t('Careers'),
-        // href: 'https://docs.pancakeswap.finance/hiring/become-a-chef',
-      // },
     ],
   },
 ]
